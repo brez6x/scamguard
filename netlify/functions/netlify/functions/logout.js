8c2f52dfa4eeeec014bd105507bd424d7df818ab
brez6x <asalem.9381@gmail.com>
2026-08-24 21:40:15 +0400

    clear old files

--- netlify/functions/netlify/functions/logout.js
+++
@@ -1,6 +0,0 @@
-const { json, clearSessionCookie } = require('./_utils');
-
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') return json(405, { error: 'Method not allowed' });
-  return json(200, { ok: true }, { 'Set-Cookie': clearSessionCookie() });
-};
