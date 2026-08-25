90b21b60bd4d74c6a8496b93cbf2c69b288a0bc9
brez6x <asalem.9381@gmail.com>
2026-08-25 21:27:40 +0400

    clean start

--- logout.js
+++
@@ -1,6 +0,0 @@
-const { json, clearSessionCookie } = require('./_utils');
-
-exports.default = async (req) => {
-  if (req.method !== 'POST') return json(405, { error: 'Method not allowed' });
-  return json(200, { ok: true }, { 'Set-Cookie': clearSessionCookie() });
-};
